c6b30ec18e84a546b9f4d94f55e79c92d89c3ebe
Rob Davis <login@therobyouknow.com>
2025-07-16 22:10:38 +0100

    new theme and gin admin theme

---
+++ web/themes/custom/customsolent/js/customsolent.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * customsolent behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.customsolent = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
