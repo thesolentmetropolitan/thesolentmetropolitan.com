cd5f7ae3d657662d0fda5be43d87ee24a74a19ad
Rob Davis <login@therobyouknow.com>
2025-10-05 12:20:42 +0100

    debugging to work out to change visual state, toggling

--- web/themes/custom/customsolent/js/customsolent.js
+++ web/themes/custom/customsolent/js/customsolent.js
@@ -14,17 +14,35 @@
 
         menuItems.forEach(item => {
           item.addEventListener('click', function () {
-                    console.log('main menu item clicked');
+            console.log('main menu item clicked');
             // Find the submenu within the clicked item
             const submenu = this.querySelector('.sub-menu-container');
             if (submenu) {
-                    console.log('and yes there is a submenu');
-submenu.classList.remove("hidden");
-submenu.classList.add("visible");
+
+              console.log("submenu is of type " + typeof submenu);
+
+              const allSubMenus = document.querySelectorAll('.sub-menu-container');
+
+              allSubMenus.forEach(aSubMenu => {
+                console.log("search for current - submenu is of type " + typeof aSubMenu);
+                if (aSubMenu.isEqualNode(submenu)) {
+
+
+                  console.log('found the clicked submenu');
+                }
+
+              });
+
+              /* https://stackoverflow.com/a/59906259/227926 */
+              /* Array.from(nodes).find(node => node.isEqualNode(nodeToFind)); */
+              /*const elementToRemove = Array.from(topMainContainers).find(node => node.isEqualNode(submenu));*/
+
+              console.log('and yes there is a submenu');
+              submenu.classList.remove("hidden");
+              submenu.classList.add("visible");
             }
 
           });
-
 
         });
         //console.log('It works!');
