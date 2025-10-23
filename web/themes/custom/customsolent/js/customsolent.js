bc5e3736ccb4bdccdb9b63f52121de164f7c6856
Rob Davis <login@therobyouknow.com>
2025-10-23 01:05:58 +0100

    clean up

--- web/themes/custom/customsolent/js/customsolent.js
+++ web/themes/custom/customsolent/js/customsolent.js
@@ -3,63 +3,37 @@
  * customsolent behaviors.
  */
 (function ($, Drupal) {
-
   'use strict';
-
   Drupal.behaviors.customsolent = {
     attach: function (context, settings) {
+
+      /*
+        desktop height constants
+      */
+      const submenu_desktop_hide_position = "110px";
+      const submenu_desktop_hide_position_top = "-5px";
+      const desktop_offset_height = 100;
+
       $(document).ready(function () {
 
-                $(window).resize(menu_refreshSize);
-
-        // or https://stackoverflow.com/a/24296220/227926
-        //const mainMenuNavContainer = document.querySelector('block-customsolent-mainnavigation');
-
-
-
-
+        $(window).resize(menu_refreshSize);
         const allSubMenus = document.querySelectorAll('.sub-menu-container');
-
         allSubMenus.forEach(aSubMenu => {
           submenu_hide(aSubMenu);
-          /*
-          aSubMenu.classList.remove("visible");
-          aSubMenu.classList.add("hidden-2l");
-
-
-          aSubMenu.setAttribute("style", "top: 30px");
-          */
         }
         );
-
 
         const mainMenuTopLevelButtons = document.querySelectorAll('.main-menu-item-container > * button');
 
         mainMenuTopLevelButtons.forEach(topLevelButton => {
           topLevelButton.addEventListener('click', function (e) {
-
-
-            /*
-            e.preventDefault();
-            */
-
             console.log('main menu item clicked');
             // Find the submenu within the clicked item
 
             const subMenuContainerForClickedButton = topLevelButton.parentElement.querySelector('.sub-menu-container');
 
-            //const submenu = this.querySelector('.sub-menu-container');
             if (subMenuContainerForClickedButton) {
-
-              //console.log("submenu is of type " + typeof submenu);
-
-              /*const allSubMenus = document.querySelectorAll('.sub-menu-container');*/
-
-
               const allSubMenuContainers = document.querySelectorAll('.main-menu-item-container > * .sub-menu-container');
-
-
-              //const allSubMenus = document.querySelectorAll('nav[role=navigation] button');
 
               allSubMenuContainers.forEach(aSubMenu => {
 
@@ -67,98 +41,38 @@
                   e.stopPropagation();
                   e.preventDefault();
 
-
                   console.log('found the clicked submenu');
-
-                  /*console.log('and yes there is a submenu');*/
-
-
-
                   console.log("search for current - submenu is of type " + typeof aSubMenu);
 
-                  //const aSubMenu = this.querySelector('.sub-menu-container');
-
                   const primaryMenuContainerAsId = document.getElementById("sub-menu-item-container-ul");
-
 
                   var primaryMenuContainerStyle = window.getComputedStyle(primaryMenuContainerAsId);
                   var primaryMenuContainerCssDisplay = primaryMenuContainerStyle.getPropertyValue('display');
                   console.log(primaryMenuContainerCssDisplay);
 
-                  //if (primaryMenuContainerCssDisplay == "block") {
-if (screen.width < 992) {
-
+                  if (screen.width < 992) {
                     console.log('mobile mode');
-
-                    //const aSubMenu = item.parentElement.querySelector('.sub-menu-container');
-
                     if (aSubMenu.classList.contains("hidden-2l")) {
                       submenu_show(aSubMenu);
                     }
                     else {
                       if (aSubMenu.classList.contains("visible-2l")) {
                         submenu_hide(aSubMenu);
-                        /*
-                        aSubMenu.classList.remove("visible");
-                        aSubMenu.classList.add("hidden-2l");
-                        */
                       }
                     }
-
                   }
                   else {
                     console.log('desktop mode');
 
                     if (aSubMenu.classList.contains("hidden-2l")) {
-
-                      /*
-                      
-                      .navigation__link--selected .navigation__link__down-icon
-                      Specificity: (0,2,0)
-                       {
-                          transform: rotate(-180deg);
-                      }
-                      
-                      */
-
-                      /* opening submenu */
-                      /*
-                      aSubMenu.classList.remove("hidden-2l");
-                      aSubMenu.classList.add("visible");
-                      */
-                      /*
-                      aSubMenu.one("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend",
-                        function (event) {
-                          aSubMenu.setAttribute("style", "z-index: 0");
-                          // Do something when the transition ends
-                        });
-                      */
-
-                      /* https://stackoverflow.com/a/11845715/227926 */
-                      /*
-                      aSubMenu.addEventListener('webkitTransitionEnd', transitionEndHandlerFunc_submenu_show, false);
-                      aSubMenu.addEventListener('otransitionend', transitionEndHandlerFunc_submenu_show, false);
-                      aSubMenu.addEventListener('oTransitionEnd', transitionEndHandlerFunc_submenu_show, false);
-                      aSubMenu.addEventListener('msTransitionEnd', transitionEndHandlerFunc_submenu_show, false);
-                      aSubMenu.addEventListener('transitionend', transitionEndHandlerFunc_submenu_show, false);
-                      */
-
                       submenu_show(aSubMenu);
 
                       console.log("aSubMenu is of type " + typeof aSubMenu);
 
-
-
-                      /*
-                      aSubMenu.setAttribute("style", "top: 110px");
-                      */
-                      aSubMenu.style.setProperty("top", "110px");
+                      aSubMenu.style.setProperty("top", submenu_desktop_hide_position);
 
                       /* https://www.reddit.com/r/webdev/comments/n3fijk/change_zindex_of_transitioning_element/ */
                       /* https://jonsuh.com/blog/detect-the-end-of-css-animations-and-transitions-with-javascript/ */
-
-
-
 
                       /* https://stackoverflow.com/a/15615701/227926 */
 
@@ -167,28 +81,18 @@ if (screen.width < 992) {
                       console.log('offsetHeight is of type' + typeof offsetHeight);
                       console.log('offsetHeight value is ' + offsetHeight);
 
-                      /* var offsetHeightCalc = parseInt(offsetHeight.value) + 100; */
-
-                      var offsetHeightCalc = parseInt(offsetHeight) + 100;
+                      var offsetHeightCalc = parseInt(offsetHeight) + desktop_offset_height;
 
                       console.log('offsetHeightCalc is of type ' + typeof offsetHeightCalc);
                       console.log('offsetHeightCalc value is ' + offsetHeightCalc);
 
                       const mainMenuNavContainer = document.querySelector('header > * nav[role=navigation]');
-                      //const mainMenuNavContainer = document.getElementById('block-customsolent-mainnavigation');
 
                       mainMenuNavContainer.setAttribute("style", "height: " + offsetHeightCalc + "px");
                     }
                     else {
-                      /* closing submenu */
                       if (aSubMenu.classList.contains("visible-2l")) {
-
                         submenu_hide(aSubMenu);
-                        /*
-                        aSubMenu.classList.remove("visible");
-                        aSubMenu.classList.add("hidden-2l");
-                        aSubMenu.setAttribute("style", "top: 30px");
-                        */
 
                         /* https://stackoverflow.com/a/15615701/227926 */
 
@@ -197,9 +101,7 @@ if (screen.width < 992) {
                         console.log('offsetHeight is of type' + typeof offsetHeight);
                         console.log('offsetHeight value is ' + offsetHeight);
 
-                        /* var offsetHeightCalc = parseInt(offsetHeight.value) + 100; */
-
-                        var offsetHeightCalc = parseInt(offsetHeight) - 100;
+                        var offsetHeightCalc = parseInt(offsetHeight) - desktop_offset_height;
 
                         console.log('offsetHeightCalc is of type ' + typeof offsetHeightCalc);
                         console.log('offsetHeightCalc value is ' + offsetHeightCalc);
@@ -214,54 +116,23 @@ if (screen.width < 992) {
                       }
                     } /* desktop hide-show toggle */
                   } /* detect mobile width */
-
-
-
-
                 }
                 else {
                   submenu_hide(aSubMenu);
-
-                  /*
-                  aSubMenu.classList.add("hidden-2l");
-                  aSubMenu.classList.remove("visible");
-                            aSubMenu.setAttribute("style", "top: 30px");
-*/
                 }
-
-
-
-
-
-
               });
-
               /* https://stackoverflow.com/a/59906259/227926 */
-              /* Array.from(nodes).find(node => node.isEqualNode(nodeToFind)); */
-              /*const elementToRemove = Array.from(topMainContainers).find(node => node.isEqualNode(submenu));*/
-
-              /*
-              console.log('and yes there is a submenu');
-              submenu.classList.remove("hidden-2l");
-              submenu.classList.add("visible");
-              */
             }
-
           });
-
         });
-        //console.log('It works!');
-
       });
 
       function submenu_show(aSubMenu) {
-
         aSubMenu.removeEventListener('webkitTransitionEnd', transitionEndHandlerFunc_submenu_hide, false);
         aSubMenu.removeEventListener('otransitionend', transitionEndHandlerFunc_submenu_hide, false);
         aSubMenu.removeEventListener('oTransitionEnd', transitionEndHandlerFunc_submenu_hide, false);
         aSubMenu.removeEventListener('msTransitionEnd', transitionEndHandlerFunc_submenu_hide, false);
         aSubMenu.removeEventListener('transitionend', transitionEndHandlerFunc_submenu_hide, false);
-
 
         aSubMenu.addEventListener('webkitTransitionEnd', transitionEndHandlerFunc_submenu_show, false);
         aSubMenu.addEventListener('otransitionend', transitionEndHandlerFunc_submenu_show, false);
@@ -273,18 +144,14 @@ if (screen.width < 992) {
         aSubMenu.classList.add("visible-2l");
       }
 
+
       function transitionEndHandlerFunc_submenu_show(event) {
-        /* aSubMenu.setAttribute("style", "z-index: 0"); */
         /* https://stackoverflow.com/a/68839284/227926 */
-        /*
-        aSubMenu.style.setProperty("z-index", "-1");
-        */
         event.target.style.setProperty("z-index", "0");
       }
 
+
       function submenu_hide(aSubMenu) {
-
-
         aSubMenu.removeEventListener('webkitTransitionEnd', transitionEndHandlerFunc_submenu_show, false);
         aSubMenu.removeEventListener('otransitionend', transitionEndHandlerFunc_submenu_show, false);
         aSubMenu.removeEventListener('oTransitionEnd', transitionEndHandlerFunc_submenu_show, false);
@@ -299,36 +166,30 @@ if (screen.width < 992) {
 
         aSubMenu.classList.add("hidden-2l");
         aSubMenu.classList.remove("visible-2l");
-        aSubMenu.setAttribute("style", "top: -5px"); /* top hidden position */
-        /*aSubMenu.setAttribute("style", "z-index: -1");*/
-
+        aSubMenu.setAttribute("style", "top: " + submenu_desktop_hide_position_top); /* top hidden position */
       }
-
-
 
 
       function transitionEndHandlerFunc_submenu_hide(event) {
         /* aSubMenu.setAttribute("style", "z-index: 0"); */
         /* https://stackoverflow.com/a/68839284/227926 */
-        /*
-        aSubMenu.style.setProperty("z-index", "-1");
-        */
         event.target.style.setProperty("z-index", "-1");
       }
+
 
       function menu_refreshSize() {
         var mainMenuNavContainer = document.querySelector('header > * nav[role=navigation]');
         if (!isMobile()) {
-          mainMenuNavContainer.setAttribute("style", "height: 100px");
+          mainMenuNavContainer.setAttribute("style", "height: " + desktop_offset_height + "px");
         }
         else {
           mainMenuNavContainer.setAttribute("style", "height: auto");
         }
       }
 
+
       function isMobile() {
         var primaryMenuContainerAsId = document.getElementById("sub-menu-item-container-ul");
-
         var primaryMenuContainerStyle = window.getComputedStyle(primaryMenuContainerAsId);
         var primaryMenuContainerCssDisplay = primaryMenuContainerStyle.getPropertyValue('display');
         var isMobileFlag = false;
@@ -338,10 +199,8 @@ if (screen.width < 992) {
         else {
           isMobileFlag = false;
         }
-
         return isMobileFlag;
       }
-
     }
   };
 
